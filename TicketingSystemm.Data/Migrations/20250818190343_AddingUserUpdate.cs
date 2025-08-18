1607919bbb877c280a10782b82fe8ec1e2639628
AliDweik <ali2dweik3@gmail.com>
2025-08-18 22:04:25 +0300

    Last Migrations

---
+++ TicketingSystemm.Data/Migrations/20250818190343_AddingUserUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketingSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingUserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserImagePath",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserImagePath",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
